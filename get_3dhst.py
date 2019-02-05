10ffe2e72b59fac3c2bf148fbc3f8e250200dba3
Alvaro Orsi <aaorsi@gmail.com>
2019-02-05 12:22:23 -0300

    more changes

--- get_3dhst.py
+++ get_3dhst.py
@@ -23,11 +23,11 @@ def load_3DHST_cats(RootDir='/home/CEFCA/aaorsi/work/3D-HST/'):
 
 
 
-def select_3DHST_z(zmin, zmax, LineName=None, LineMin=-1):
+def select_3DHST_z(zmin, zmax, LineName=None, LineMin=-1, RootDir=False):
 # Routine to select objects from 3D-HST based on redshift and, alternatively, a particular line above a line flux
 
-
-  dcat, lcat = load_3DHST_cats()
+    
+  dcat, lcat = load_3DHST_cats(RootDir) if RootDir else load_3DHST_cats() 
   
 # dcat IDs start at 1, dcat[id][0] = 1
 
