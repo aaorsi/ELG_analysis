ad1041a683d638b884922b443ac683e8ac6dd6c0
Alvaro Orsi <aaorsi@gmail.com>
2019-04-19 09:56:36 +1200

    more updates

--- py/keras_elgs.py
+++ py/keras_elgs.py
@@ -22,14 +22,11 @@ training_class    = np.asarray(dset['tclass'])
 test_features     = np.asarray(dset['vfeatures'])
 test_class        = np.asarray(dset['vclass'])
 
-
 x_train, x_test, y_train, y_test = train_test_split(
-training_features, training_class, test_size=0.1, random_state=None)  # leave out 10% out for test/validation, whatever that's called
-
+training_features, training_class, test_size=0.2, random_state=None)  # leave out 10% out for test/validation, whatever that's called
 
 print 'Unique classes in training set: ',np.unique(y_train)
 print 'Number of objects in training set:', len(y_train)
-
 
 Scaledata = True
 if Scaledata:
@@ -39,8 +36,6 @@ if Scaledata:
     x_train = scaler.transform(x_train)
     x_test  = scaler.transform(x_test)
     print 'done'
-
-
 
 nfeat = len(x_train[0])
 print nfeat
@@ -52,9 +47,9 @@ def class_to_int(istr):
     if istr == 'Halpha':
         return 0
     if istr == 'OII':
-        return 2
-    elif istr == 'OIII+Hbeta':
         return 1
+    elif istr == 'OIII+Hbeta':
+        return 2
     elif istr == 'contaminant':
         return 3
     else:
@@ -63,7 +58,7 @@ def class_to_int(istr):
 
 
 y_train_int = np.asarray([class_to_int(x) for x in y_train])
-y_test_int = [class_to_int(x) for x in y_test]
+y_test_int  = [class_to_int(x) for x in y_test]
 
 
 y2_train = keras.utils.to_categorical(y_train_int, num_classes=len(np.unique(y_train_int)))
@@ -87,11 +82,11 @@ from keras.regularizers import l2
 def build_classifier(optim='sgd',drop=0.0,Neurons=5,init='uniform',lambd=0.0):
    #   from keras.optimizers import SGD
     model = Sequential()
-    model.add(Dense(Neurons, input_dim=nfeat, kernel_initializer=init, activation='relu', W_regularizer=l2(lambd)))
-    #model.add(Dropout(drop))
-    #model.add(Dense(Neurons, kernel_initializer=init, activation='relu', W_regularizer=l2(lambd)))
-    #model.add(Dropout(drop))
-    model.add(Dense(4, kernel_initializer=init, activation='relu',W_regularizer=l2(lambd)))
+    model.add(Dense(Neurons, input_dim=nfeat, kernel_initializer=init, activation='sigmoid', W_regularizer=l2(lambd)))
+    model.add(Dropout(drop))
+    model.add(Dense(Neurons, kernel_initializer=init, activation='sigmoid', W_regularizer=l2(lambd)))
+    model.add(Dropout(drop))
+    model.add(Dense(4, kernel_initializer=init, activation='sigmoid',W_regularizer=l2(lambd)))
     # Compile model
     model.compile(loss='sparse_categorical_crossentropy', optimizer=optim, metrics=['accuracy'])
     return model
@@ -103,10 +98,10 @@ model = KerasClassifier(build_fn=build_classifier, verbose=0)
 
 optim = ['sgd']#,'adam']#,'rmsprop','sgd'] #['rmsprop', 'adam']
 init = ['uniform']#['glorot_uniform', 'normal', 'uniform']
-epochs = [10]#,20]
+epochs = [10,20]
 batches = [200]#,200,500]#,500]#, 500]
-lambd   = [1e-3,1e-2]#,1e-1]#,5e-3,1e-3]
-dropout = [0]#1e-3,1e-2,1e-1]#,0.5,0.75]
+lambd   = [0]#,1e-1]#,5e-3,1e-3]
+dropout = [0,.1]#1e-3,1e-2,1e-1]#,0.5,0.75]
 Neurons = [20]
 param_grid = dict(optim=optim, epochs=epochs, batch_size=batches, init=init, lambd=lambd,drop=dropout,
                  Neurons=Neurons)
